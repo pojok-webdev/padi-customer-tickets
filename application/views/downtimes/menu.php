81c80cc437e1b1a682889074330b4613b5555439
pujie <pw.prayitno@gmail.com>
2020-04-10 12:59:43 +0700

    remove unused

--- application/views/downtimes/menu.php
+++ application/views/downtimes/menu.php
@@ -10,8 +10,6 @@
             <img src="/asset/aqua/img/users/aqvatarius.jpg" class="img-polaroid"/>                
         </div>
         <ul class="control">                
-            <li><span class="icon-comment"></span> <a href="#">Messages</a> <a href="#" class="caption red">12</a></li>
-            <li><span class="icon-cog"></span> <a href="#">Settings</a></li>
             <li><span class="icon-share-alt"></span> <a href="/main/logout">Logout</a></li>
         </ul>
         <div class="info">
