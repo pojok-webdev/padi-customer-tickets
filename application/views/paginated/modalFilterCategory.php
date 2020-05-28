9410f4721d23f250d2716f6e96ee30757b282f56
pujie <pw.prayitno@gmail.com>
2020-05-28 17:02:16 +0700

    add filter by category dialog

---
+++ application/views/paginated/modalFilterCategory.php
@@ -0,0 +1,51 @@
+<div id="mFilterCategory" class="modal hide fade" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal-header">
+        <h3 id="myModalLabel"> Filter</h3>
+    </div>
+    <div class="row-fluid">
+        <div class="block-fluid">
+            <div class="row-form clearfix">
+                <div class="span3">Tahun:</div>
+                <div class="span9">
+                    <select id="yearfilter">
+                        <option value="All">All</option>
+                        <option value="2020">2020</option>
+                        <option value="2019">2019</option>
+                        <option value="2018">2018</option>
+                    </select>
+                </div>
+            </div>            
+
+
+            <div class="row-form clearfix">
+                <div class="span3">Tahun:</div>
+                <div class="span9">
+                    <label class="checkbox inline">
+                        <input type="checkbox" value="checkbox1"/> unchecked
+                    </label>
+                </div>
+            </div>            
+
+
+            <div class="row-form clearfix">
+            <div class="span3">Category:</div>
+                <div class="span9">
+                    <select id="categoryfilter">
+                        <option value="All">All</option>
+                        <option value="FFR">FFR</option>
+                        <option value="Platinum">Platinum</option>
+                        <option value="Gold">Gold</option>
+                        <option value="Bronze">Bronze</option>
+                        <option value="Silver">Silver</option>
+                        <option value="Uncategorized">Uncategorized</option>
+                    </select>
+                </div>
+            </div>
+        </div>                
+    </div>
+    <div class="modal-footer">
+        <button class="btn btn-warning" data-dismiss="modal" aria-hidden="true" id="searchbyfilter">Search</button>
+        <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>            
+    </div>
+
+</div>
