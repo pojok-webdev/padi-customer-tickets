c0e4051d939edd09ee3e71116c14d489c30461fc
pujie <pw.prayitno@gmail.com>
2021-02-19 09:49:24 +0700

    create folders in ftp client

--- application/controllers/folders.php
+++ application/controllers/folders.php
@@ -10,7 +10,7 @@ Class Folders extends CI_Controller{
         foreach($objs['res'] as $cln){
             echo $cln->name;
             echo '<br />';
-            $this->create($cln->name);
+            $this->ftpcreatedir($cln->name);
         }
     }
     function create($client){
@@ -18,9 +18,34 @@ Class Folders extends CI_Controller{
             echo "Cant create folder";
         }
     }
-    function ftpmkdir(){
-        $dir = 'Task%20List/Sales/Puji%20Widi%20P/directories';
+    function ftpcreatedir($dir){
+        $dir = 'Task%20List/Sales/Puji%20Widi%20P/directories/x/'.$dir;
+        $dir = 'Task List/Sales/Puji Widi P/directories/x/'.$dir;
         $ftp_server = "192.168.0.234";
+        $ftp_user_name = 'puji';
+        $ftp_user_pass = 'puj12020';
+        // set up basic connection
+        $conn_id = ftp_connect($ftp_server);
+
+        // login with username and password
+        $login_result = ftp_login($conn_id, $ftp_user_name, $ftp_user_pass);
+
+        // try to create the directory $dir
+        if (ftp_mkdir($conn_id, $dir)) {
+        echo "successfully created $dir\n";
+        } else {
+        echo "There was a problem while creating $dir\n";
+        }
+
+        // close the connection
+        ftp_close($conn_id);
+    }
+    function ftpmkdir(){
+        $dir = 'Task%20List/Sales/Puji%20Widi%20P/directories/x';
+        $dir = 'Task List/Sales/Puji Widi P/directories/x';
+        $ftp_server = "192.168.0.234";
+        $ftp_user_name = 'puji';
+        $ftp_user_pass = 'puj12020';
         // set up basic connection
         $conn_id = ftp_connect($ftp_server);
 
